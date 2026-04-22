e00c9e39feb6ea96e9eea0b89c33a801ecd38ee4
Mohankumar Muthusamy <akpmohan07@gmail.com>
2026-04-22 18:23:11 +0100

    fix: add missing Google OAuth userinfo scopes for calendar setup (#5)

--- apps/api/src/services/calendar-providers/google.ts
+++ apps/api/src/services/calendar-providers/google.ts
@@ -26,6 +26,8 @@ const GOOGLE_CALENDAR_API = 'https://www.googleapis.com/calendar/v3';
 const SCOPES = [
   'https://www.googleapis.com/auth/calendar.readonly',
   'https://www.googleapis.com/auth/calendar.events',
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/userinfo.profile'
 ].join(' ');
 
 export class GoogleCalendarProvider implements CalendarProvider {
